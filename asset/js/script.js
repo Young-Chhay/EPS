3f92f00c8e55a85becd3faf37855290719703d4e
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-08-16 19:37:22 -0700

    fetch test

--- asset/js/script.js
+++ asset/js/script.js
@@ -2,3 +2,30 @@ console.log(api.key);
 
 console.log(`https://api.yelp.com/v3/businesses/search`);
 
+// Searchbar function
+var searchFormEl = $('#search-form');
+
+function handleSearchFormSubmit(e) {
+  e.preventDefault();
+
+  var searchInputVal = $('#search-input').val();
+
+  if (!searchInputVal) {
+    console.log('You need a search input value!');
+    return;
+  }
+
+  var city = searchInputVal;
+  searchApi(city);
+}
+
+searchFormEl.submit(handleSearchFormSubmit);
+
+// Call Yelp Api for Top Outdoor Restaurant 
+
+var Outdoors = [{
+    name: "Waterpark",
+    inout: "outdoors",
+    yelpID: "quroiwefjlk43ef34",
+},
+]
