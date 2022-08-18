ac362d0ce2c2e01e62f75b6c187cac8a53ec6519
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-08-17 19:10:12 -0700

    restaurant and hotel fetch, display restaurant results to HTML

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,9 +3,17 @@ console.log(api.key);
 console.log(`https://api.yelp.com/v3/businesses/search`);
 
 // Moment statement for day and time 
+
 moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 // variables
+var cityRestaurantEl = $('#city-restaurant')
+var firstEatNameEl = $("#first-eat-name")
+var firstEatImgEl = $("#first-eat-img")
+var firstEatRatingEl = $("#first-eat-rating")
+var firstEatPriceEl = $("#first-eat-price")
+var firstEatUrlEl = $("#first-eat-url")
+
 // var searchButton = $(".searchButton")
 // var apiKey = ""
 // var searchInput = ""
@@ -77,6 +85,88 @@ function indoorYelpApi(city) {
 }
 
 
+function restaurantYelpApi(city) {
+  
+  var restaurantSearchUrl = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=' + city + '&categories=restaurants&sort_by=rating'
+
+  console.log(restaurantSearchUrl)
+
+  fetch(restaurantSearchUrl, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + api.key,
+      'Access-Control-Allow-Origin': '*', 
+    },
+  })
+  .then(function (response) { 
+  if (response.ok) {
+      response.json().then(function (eatResults) {
+      console.log(eatResults);
+      renderRestaurants(eatResults)
+      });
+  } else {
+      alert('Error: ' + response.statusText);
+  }
+  })
+  .catch(function (e) {
+  console.log(e);
+  });
+  }
+
+function hotelYelpApi(city) {
+  
+  var hotelSearchUrl = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=' + city + '&categories=hotels&sort_by=rating'
+
+  console.log(hotelSearchUrl)
+
+  fetch(hotelSearchUrl, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + api.key,
+      'Access-Control-Allow-Origin': '*', 
+    },
+  })
+  .then(function (response) { 
+  if (response.ok) {
+      response.json().then(function (stayResults) {
+      console.log(stayResults);
+      });
+  } else {
+      alert('Error: ' + response.statusText);
+  }
+  })
+  .catch(function (e) {
+  console.log(e);
+  });
+  }
+  // hotelYelpApi("Irvine");
+restaurantYelpApi("Irvine");
+
+function renderRestaurants (eatResults) {
+  var firstEatName = eatResults.businesses[0].name
+  var firstEatImg = eatResults.businesses[0].image_url
+  var firstEatRating = eatResults.businesses[0].rating
+  var firstEatPrice = eatResults.businesses[0].price
+  var firstEatUrl = eatResults.businesses[0].url
+  
+  // Plug city name into Eat Card (placeholder of Irvine)
+  cityRestaurantEl.text(`Top Rated Restaurants in Irvine`)
+
+  // Plug name into Eat Card 
+  firstEatNameEl.text(firstEatName);
+
+  // Plug image URL into Eat Card 
+  firstEatImgEl.attr('src',firstEatImg); 
+
+  //  Clicking image should link to Yelp review
+  firstEatUrlEl.attr('href', firstEatUrl);
+
+  // Plug rating into Eat Card
+  firstEatRatingEl.text(`Rating: ${firstEatRating}`);
+
+  // Plug price into Eat Card
+  firstEatPriceEl.text(`Price: ${firstEatPrice}`);
+}
 
 // var cityResponse = [
 //     {
