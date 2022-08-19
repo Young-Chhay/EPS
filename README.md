9f703577e7efa4714436360e89c084317b2f019b
Kevin Ng <kevinng3292@gmail.com>
2022-08-18 21:58:14 -0700

    new readme & weather api functionality

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# EPS
+# Eat Play Stay
+## Link's
+* ['Eat Play Stay -Github'](git hub link)
+* ['Eat Play Stay - Live Link] (EPS live link)
+
+<!-- screen shots here -->
+
+## Description
+Planning your next vacation? Need to step away from work for a bit? Eat Play Stay will help you get the results you need. Find the best hotels, restaurants, activites and weather forecasts by location and get away.
+
+Eat Play Stay is a search data base where users can plan their vacations using the openweather api forecast and yelp finder search features. EPS allows user to easily navigate for hotels, activites to do, restaurants to eat and knows the weather ahead of time to plan when is the best time to go. 
+
+## Code Features:
+* HTML
+* CSS
+* Bulma
+* Jquery
+* Javascript
+* OpenWeather API
+* Yelp Review API
+
+## Process:
+This group project was collaborated by Jeffrey Sun, Young Chhay, Kevin Ng & Drum Holliday  The tasks were divded equally amongst all members.
+* Jeffrey researched the Yelp API keys and designed the functionality in the JSS.
+* Young orchestrated the Github structure, Terminal push/pulls and Math.random functions.
+* Kevin contributed to the framework with the html + basic styling in css as well as the weather API key functionality.
+* Drum provided the pictures as well as styling in css & bulma, and also made edits and corrections in the JSS.
+  
+Although the project seemed overwhelming at first, everyone helped each other and filled in the gaps when needed. Our team work well together and was very attentive to the details with communication and persistence. 
+
+## Challenges 
+* Resolving Github merge conflicts 
+* Limited Free API's available for travel searches
+
+## User Story
+* As A Traveler
+* I want to see the weather forecast for cities when I search for them so I can plan a trip accordingly
+* When I search for a city,
+* Then I am presented with current and 5 day weather conditions for that city and that city is added to the search history.
+* When the city is loaded,
+* Then Yelp will load review into 3 categories with pictures that have live links to take me to a Yelp Review website
+  * Top #1 Rated Restaurant
+  * Top #1 Activity
+  * Top #1 Hotels to Stay
+
+## Collabortors
+* Jeffrey Sun
+* Young Chhay
+* Kevin Ng
+* Drum Holliday
+
+## Resources Used
+* Bulma Documentation
+* Yelp.com API
+* OpenWeather API
--- asset/js/second-page.js
+++ asset/js/second-page.js
@@ -2,120 +2,163 @@
 moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
-// var resultTextEl = document.querySelector('#result-text');
-// var resultContentEl = document.querySelector('#result-content');
-// var searchFormEl = document.querySelector('#search-form');
-
-
 
 console.log(api.key);
 
 console.log(`https://api.yelp.com/v3/businesses/search`);
 
-// Variables 
+// open weather api
+// variables
+var searchButton = $("btn");
 var apiKey = "d4336a7fa9f7843bf43a971a6d63d529";
+var searchInput = "";
+var liveWeather = $(".weathericon");
+var liveTempEl = $("#todaytemp");
+var currentDate = moment().format("M/D/YYYY");
+var cityName = "";
+var todayContainer = $("todaycontainer");
+// 5 day forecast display
+var dailyDivs = [$('#day-1-div'), $('#day-2-div'), $('#day-3-div'), $('#day-4-div'), $('#day-5-div')];
 
-var searchButton = $(".searchButton");
+var savedCities =JSON.parse(localStorage.getItem('savedCities')) || [];
 
-// local storage counting city names
-var cityCount = 0;
+// var weathersearchUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
 
 
-// city names being loading onto HTML page using Forloop
-for (var i = 0; i < localStorage.length; i++) {
+// loading the data onto HMTL page using ForLoop
+for (var i = 0; i < savedCities.length; i++) {
 
-    var city = localStorage.getItem(i);
+    var city = savedCities[i];
+    var cityNameEl = $("<li>");
+    cityNameEl.addClass("list-group-item btn btn-primary col-12 btn-style btn-recent");
+    cityNameEl.text(city);
 
-    var cityName = $(".list-cities");
-
-    cityName.append("<li>" + city + "</li>");
+    $(".list-group").append(cityNameEl);
 }
 
+// Key count for local storage 
+var cityCount = 0;
+// Search button click event
+document.getElementById("formid").addEventListener('submit', function(event){
+    event.preventDefault();
+    searchInput = $(".input").val();
+    var weatherContainer = document.getElementById("weather")
+    weather.scrollIntoView()
 
+        // Storing New Cities into local storage with the old Cities
+        var previouslySavedCities = JSON.parse(localStorage.getItem("savedCities")) || []
+        previouslySavedCities.push(searchInput)
+        localStorage.setItem("savedCities", JSON.stringify(previouslySavedCities))
 
-// Search button feature
-searchButton.click(function () {
-
-    var searchCity = $(".searchCity").val();
-    
-
-    // live weather
-    var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&Appid=" + apiKey + "&units=imperial";
-
-
-    if (searchCity == "") {
-        // console.log(searchCity);
-    } else {
-        $.ajax({
-            method: "GET",
-            url: currentWeather
-           
-        }).then(function (response) {
-            // console.log(response.name);
-            var cityName = $(".list-cities");
-            cityName.append("<li>" + response.name + "</li>");
-            // Local storage
-            var local = localStorage.setItem(cityCount, response.name);
-            cityCount = cityCount + 1;
-
-            // current weather data 
-            var currentForecast = $(".currentForecast").append("<div>");
-            currentForecast.empty();
-            var currentCity = currentForecast.append("<p>");
-            currentForecast.append(currentCity);
-
-            // Adjust Date 
-            var timeUTC = new Date(response.dt * 1000);
-            currentCity.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
-            currentCity.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
-            // temperature input
-            var liveTemp = currentCity.append("<p>");
-            currentCity.append(liveTemp);
-            liveTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
-
-    
-        });
-
-       
-        
-    }
 });
 
+function getSavedCityWeather() {
+    getUserLocation($(this).text())
+}
 
-// variables
-// var searchButton = $(".searchButton")
-// var apiKey = ""
-// var searchInput = ""
-// var currentTempEl = $("#todaytemp")
-// var currentUvEl = $("#uvindex")
-// var currentDate = moment().format("M/D/YYYY")
-// var cityName = ""
-// var savedCities = JSON.parse(localStorage.getItem("savedCities")) || []
-// var previousSearch = document.querySelector("#previous-search")
+// get city search for user location
+function getUserLocation(city) {
+        // Make fetch request
+    // Get location lon and lat
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey).then(function(response) {
+        
+        // Check for valid response
+        if (response.ok) {
+            response.json().then(function(data) {
+                
+                // Gets the lon and lat of the city
+                var locationLat = data[0].lat;
+                var locationLon = data[0].lon;
+                cityName = data[0].name;
+                // Convert from Int to String
+                var latString = locationLat.toString();
+                var lonString = locationLon.toString();
 
-// var cityResponse = [
-//     {
-//         city: "New York City",
-//         activity: "Central Park",
-//         restaurant: "Joe's Shanghai",
-//         // weather: "live weather API Data"
-//     },
-
-//     {
-//         city: "Los Angeles",
-//         activity: "Santa Monica Pier",
-//         restaurant: "Honey Hi",
-//         // weather: "live weather API Data"
-
-//     },
-
-//     {
-//         city: "San Diego",
-//         activity: "Torrey Pines",
-//         restaurant: "Javier's - La Jolla"
-//         // weather: "live weather API Data"
-
-//     }
-//
+                // Call function to get values
+                getLocationWeather(latString, lonString);
+            });
+        } else {
+            alert("Try Again, city not found");
+        }
+    }).catch(function(error) {
+        
+        alert("Weather Unavailable");
+    });
 
 
+};
+
+function getLocationWeather(lat, lon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + apiKey;
+
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                
+                // City & date
+                var currentCityNameEl = $(".weather-title");
+                currentCityNameEl.text("Weather Forecast for " + cityName.charAt(0).toUpperCase() + cityName.slice(1));
+                // Weather Icon
+                var liveWeatherIconEl = $("#current-icon");
+                liveWeatherIconEl.attr("src", "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png")
+
+                var liveTemp = currentCity.append("<p>");
+                currentCity.append(liveTemp);
+                liveTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+
+                // Current Temp
+                var liveTempEl = $("#todaytemp");
+                liveTempEl.text(data.current.temp + "F");
+
+                var forecastarray = data.daily
+                todaycontainer.html("");
+
+                for (var i = 0; i < forecastarray.length - 2; i++) {
+                    // converting Unix to date
+                    var humanDateFormat = new Date(data.daily[i].dt * 1000).toLocaleDateString("en-US");
+
+                    // Variables to dynamically create the classes into HTML
+                    // var card = $("<div>").addClass("forecast-card");
+                    // var cardbody = $("<div>").addClass("forecast-body");
+                    // var cardheader = $("<div>").addClass("h3");
+                    // var weatheri = $("<img>").addClass("card-header-icon");
+                    // var temptext = $("<div>").addClass("h3");
+                                
+
+                    // getting the data from API
+                    cardheader.text(humanDateFormat);
+                    weatheri.attr("src", "https://openweathermap.org/img/wn/" + forecastarray[i].weather[0].icon + ".png");
+                    temptext.text("Temp: " + forecastarray[i].temp.day + " F");
+                    
+                }
+            })
+        }
+    })
+}
+
+$("#citiesList").on("submit", ".list-group-item", getSavedCityWeather)
+
+// if (searchCity == "") {
+//     // console.log(searchCity);
+// } else {
+//     $.ajax({
+//         method: "GET",
+//         url: currentWeather
+       
+//     }).then(function (response) {
+//         // console.log(response.name);
+//         var cityName = $(".list-cities");
+//         cityName.append("<li>" + response.name + "</li>");
+//         // Local storage
+//         var local = localStorage.setItem(cityCount, response.name);
+//         cityCount = cityCount + 1;
+
+//         // current weather data 
+//         var currentForecast = $(".currentForecast").append("<div>");
+//         currentForecast.empty();
+//         var currentCity = currentForecast.append("<p>");
+//         currentForecast.append(currentCity);
+//     });
+
+// };
