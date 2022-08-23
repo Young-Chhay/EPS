90870f2e8780bd25a75cc3cc1fd93b113bd05256
Young C <skykeller80@gmail.com>
2022-08-22 20:36:18 -0700

    added keys to make web works

--- asset/js/script.js
+++ asset/js/script.js
@@ -2,6 +2,14 @@
 moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
+// const yelpApi = {
+//   key : '6BU5omcQhPToYfC0TRPt0h5hsphV-BaAn78l-elZXw779LTTD76ltEou8kp75L7_A7wwuKfhfpY48qIRoKKdGHd6-Xq94c8sTdZlDuMytEwRdedCNrN8KOpTQE_8YnYx',
+// };
+
+// const weatherApi = {
+//   key : '81dbd64740ac5b0c8f53234967959bd9'
+// };
+
 var searchButton = $("btn");
 var searchInput = "";
 var switchElement = $('switch-element');
