17ae0c4693ef1eb3775c037cdc47099501253a20
Young C <skykeller80@gmail.com>
2022-08-22 20:38:48 -0700

    fixes key errors and make page load

---
+++ asset/js/config.js
@@ -0,0 +1,7 @@
+const yelpApi = {
+    key : '6BU5omcQhPToYfC0TRPt0h5hsphV-BaAn78l-elZXw779LTTD76ltEou8kp75L7_A7wwuKfhfpY48qIRoKKdGHd6-Xq94c8sTdZlDuMytEwRdedCNrN8KOpTQE_8YnYx',
+};
+
+const weatherApi = {
+    key : '81dbd64740ac5b0c8f53234967959bd9'
+};
